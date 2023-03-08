9369d05b85da030cc45558881f73b2384081e494
rolen <roahhanchaulagain@gmail.com>
2023-03-08 20:47:44 +0545

    kushal

---
+++ Python/w3/temp.py
@@ -0,0 +1,37 @@
+# fruits = {"apple", "banana", "cherry"}
+# fruits.add('mango')
+# print('fruits:', fruits)
+
+# y=fruits.copy()
+# print('y:', y)
+
+# fruits.clear()
+# print(fruits)
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+
+# print(x.difference(y))
+# print(y.difference(x))
+
+# x.difference_update(y)
+# print('updated x:', x)
+
+# x = {"apple", "banana", "cherry"}
+# x.discard('banana1')
+# x.discard('banana')
+# print(x)
+# # x.remove('banana1')  gives key error
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# print('intersection:', x.intersection(y)) # instead of y any, no of st separated by commas can be placed
+# x.intersection_update(y) # instead of y any, no of st separated by commas can be placed
+# print('this is x after intersection update:',x)
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# print('Isdisjoint:', x.isdisjoint(y))
+
+
+
