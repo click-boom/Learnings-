f236a8542dcb6c0e3d5527ca874bbcb492d6ab60
rolen <roahhanchaulagain@gmail.com>
2023-03-19 21:46:44 +0545

    Java in the house

---
+++ JAVA/book.java
@@ -0,0 +1,25 @@
+class book{
+    // public static void main(String[] args){
+    //     System.out.print("Hello java");
+    //     System.out.println();
+    // }
+    // public static void main(String[] args){
+    //     int a= 200;
+    //     int b= 100;
+    //     int c=a+b;
+    //     System.out.println("Sum is:"+ c);
+
+    // }
+    // public static void main(String[] args){
+    //     for (int i = 0; i <=5; i++) {
+    //         for (int j = 0; j <=i; j++) {
+    //             System.out.print("*");       
+    //         }
+    //     System.out.println();
+    //     }
+    // }
+    // public static void main(String[] args){
+    //     if (true) System.out.println("Its true");
+    //     System.out.println((10>9));
+    // }
+}
