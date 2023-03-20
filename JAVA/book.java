7633a9056ea336611e77e1e69d5fe187db930742
rolen <roahhanchaulagain@gmail.com>
2023-03-20 18:32:13 +0545

    upto today

--- JAVA/book.java
+++ JAVA/book.java
@@ -1,25 +1,87 @@
-class book{
-    // public static void main(String[] args){
-    //     System.out.print("Hello java");
-    //     System.out.println();
+import java.io.IOException;
+public class book {
+    // public static void main(String[] args) {
+    // System.out.print("Hello java");
+    // System.out.println();
     // }
-    // public static void main(String[] args){
-    //     int a= 200;
-    //     int b= 100;
-    //     int c=a+b;
-    //     System.out.println("Sum is:"+ c);
 
-    // }
     // public static void main(String[] args){
-    //     for (int i = 0; i <=5; i++) {
-    //         for (int j = 0; j <=i; j++) {
-    //             System.out.print("*");       
-    //         }
-    //     System.out.println();
-    //     }
+    // int example1= 200;
+    // int example2= 100;
+    // int sum_of_examples=example1+example2;
+    // System.out.println("Sum is:"+ sum_of_examples);
     // }
+
     // public static void main(String[] args){
-    //     if (true) System.out.println("Its true");
-    //     System.out.println((10>9));
+    // for (int i = 0; i <=5; i++) {
+    // for (int j = 0; j <=i; j++) {
+    // System.out.print("*");
     // }
+    // System.out.println();
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 10;
+    // int After_increment = x++;
+    // System.out.println("Y after post-increment:" + After_increment);
+    // System.out.println("X after post-increment:" + x);
+    // /*
+    // * Post means dont increment at the point it was declared, but increment for
+    // the steps from now on.
+    // * Something like while loop in C.
+    // */
+    //
+    // After_increment = ++x;
+    // System.out.println("Y after pre-increment:" + After_increment);
+    // System.out.println("X after pre-increment:" + x);
+    // /*
+    // * Pre means increment at the point it was declared.
+    // * Something like while loop in C.
+    // */
+
+    // public static void main(String[] args){
+    // if (true) System.out.println("Its true");
+    // System.out.println((10>9));
+    // }
+
+    // public static void main(String[] args){
+    // long rep_long=12L;
+    // float rep_float=12F;
+    // System.out.println("represent float and long respectively:"+rep_float,
+    // rep_long); // Says cant print long using print or println
+    //
+    // char rep_char_using_int=90;
+    // System.out.println("represent char using int:"+rep_char_using_int);
+    //
+    // int sep_underscore=10_00_000;
+    // System.out.println("separating integer values with
+    // underscore:"+sep_underscore);
+    //
+    // char single_char='a';
+    // System.out.println(single_char);
+
+    /* Scope */
+    // public static void main(String[] args){
+    // int rep_hexa = 0xFF;
+    // int rep_octal = 011;
+    // int rep_binary = 0b100;
+
+    // System.out.println("represent hexadecimal ,octal and binary respectively:" +
+    // rep_hexa + " " + rep_octal + " "
+    // + rep_binary); // Says cant print string and int together using print or
+    // println
+
+    // public static void main(String[] args) {
+        // int x;
+        // x = 10;
+        // if (x == 10) {
+        // int y = 20;
+        // System.out.println("x and y" + x + "" + y);
+        // x = y * 2;
+        // }
+        // // y = 100; Error
+        // x = 100; // No error
+
+   
 }
